a72d22bac9d9d4cfb9d39c98e380946025022fbf
Larisa Melnichenko <erlara@ukr.net>
2023-03-10 11:35:07 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,97 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+
+const initialContacts = [
+  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+      return;
+    }
+    this.setState({ contacts: initialContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    const oldContacts = contacts.map(({ name }) => name.toLowerCase());
+
+    if (oldContacts.includes(name.toLowerCase())) {
+      return alert(`${name} is already in contacs.`);
+    }
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getFilteredContacts();
+
+    return (
+      <Layout>
+        <GlobalStyle />
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} toFilter={this.changeFilter} />
+        <ContactList contacts={visibleContacts} onDelete={this.deleteContact} />
+      </Layout>
+    );
+  }
+}
